0ab42f228bafc9679f80a7af37fb18b9a2b2422a
Johan Galeano <jgaleano@ivolucion.com>
2024-08-29 20:30:38 -0500

    node

---
+++ node/02_baby-steps.js
@@ -0,0 +1,7 @@
+// console.log(process.argv)
+
+const args = process.argv.slice(2);
+
+const sum = args.reduce((total, num) => total + Number(num), 0);
+
+console.log(sum);
---
+++ node/05_filtered-ls.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = process.argv[2];
+const fileExtension = '.' + process.argv[3];
+
+fs.readdir(directoryPath, (err, files) => {
+  if (err) {
+    return console.error(err);
+  }
+
+  files
+    .filter(file => path.extname(file) === fileExtension)
+    .forEach(file => console.log(file));
+});
